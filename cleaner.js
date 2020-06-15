23b5c45a0707e3c64096eba3b6f053259397fc02
Yann Klein <yann.klein@me.com>
2020-06-16 01:37:33 +0900

    csv reading basic js

--- cleaner.js
+++ cleaner.js
@@ -1,1 +1,57 @@
-console.log("test");
+const input = document.querySelector("#csvFile");
+const comparisonArea = document.querySelector(".data-comparison");
+const imgClean = document.querySelector(".data-images-clean");
+const imgCheck = document.querySelector(".data-images-check");
+const dataClassName = document.querySelector(".data-classname");
+
+let currentLine = 0;
+const originalDataArray = [];
+const resultDataArray = [];
+
+
+const processCSV = (event) => {
+  const csvFile = event.currentTarget.files[0];
+  if (window.FileReader) {
+    // FileReader are supported.
+    currentLine = 0;
+    getAsText(csvFile);
+  } else {
+      alert('FileReader are not supported in this browser.');
+  }
+}
+
+const getAsText = (fileToRead) => {
+  const reader = new FileReader();
+  // Read file into memory as UTF-8
+  reader.readAsText(fileToRead);
+  // Handle errors load
+  reader.onload = loadHandler;
+  reader.onerror = errorHandler;
+};
+
+const loadHandler = (event) => {
+  const csv = event.target.result;
+  processData(csv);
+};
+
+const processData = (csv) => {
+  originalDataArray = csv.split(/\r\n|\n/);
+  initalizeComparison();
+  showANewImage();
+};
+
+const errorHandler = (evt)  => {
+  if(evt.target.error.name == "NotReadableError") {
+      alert("Cannot read file !");
+  }
+};
+
+const initalizeComparison = () => {
+
+};
+
+const showANewImage = () => {
+
+};
+
+input.addEventListener("change", processCSV);
